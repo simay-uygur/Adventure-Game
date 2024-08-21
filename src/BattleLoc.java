85cf1b41a0a85b5dbb79ee061624e66dc7f6aa0c
simay-uygur <simayuygur09@gmail.com>
2024-08-21 15:53:52 +0300

    combat method and getclass returning class Zombie etc corrections

--- src/BattleLoc.java
+++ src/BattleLoc.java
@@ -2,38 +2,84 @@ import java.util.Random;
 
 public class BattleLoc extends Location{
 
-    private Enemy[] enemy;
+    private Enemy[] enemy = new Enemy[3];
     private int enemyNumber;
 
     public void battleLoc(Enemy e )
     {
         String typeEnemy = (e.getClass().toString());
         int randomAmount = (int) (Math.random() * 3) + 1;
+        if(randomAmount <= 0 || randomAmount >3) randomAmount = 1;
         enemyNumber = randomAmount;
 
         switch (typeEnemy){
-            case "Zombie":
-                enemy = new Zombie[randomAmount];
-                System.out.println("Zombie/s are created");
-                break;
-            case "Vampire":
-                enemy = new Vampire[randomAmount];
-                System.out.println("Vampire/s are created");
-                break;
-            case "Bear":
-                enemy = new Bear[randomAmount];
-                System.out.println("Bear/s are created");
-                break;
+            case "class Zombie":
+                for(int i =0; i < enemyNumber; i++){
+                    enemy[i] = new Zombie();
+                }
+                System.out.println(enemyNumber+" Zombie/s are created");
+            break;
+            case "class Vampire":
+                for(int i =0; i < enemyNumber; i++){
+                    enemy[i] = new Vampire();
+                }
+                System.out.println(enemyNumber+ " Vampire/s are created");
+            break;
+            case "class Bear":
+                for(int i =0; i < enemyNumber; i++){
+                    enemy[i] = new Bear();
+                }
+                System.out.println(enemyNumber+" Bear/s are created");
+            break;
             default:
-                break;
+            break;
         }
     }
 //onlocation method (does id have to be implemented again?)   i wont use it
     public void combat(){
+        int playerDamage = getPlayer().getDamage();
+        int enemyDamage = enemy[0].getDamage();
+        System.out.println("Battle starts... ");
+        while (!getPlayer().isPlayerDead() && !isAllEnemiesDead()){
+            System.out.println("Player attacks " + playerDamage );
+            attackEnemy(-playerDamage);
+            for(int s=0; s<enemyNumber; s++){
+                System.out.println("Enemy attacks " + enemyDamage);
+                getPlayer().damagePlayer(enemyDamage);
+                if(getPlayer().isPlayerDead()){
+                    System.out.println("Player dead");  //game over
+                    return;
+                }
+            }
+        }
 
+        if(isAllEnemiesDead()){
+            System.out.println("All enemies dead. You got the gift.");
+            gift();
+        } else if(getPlayer().isPlayerDead()){
+            Game.over();
+        }
+    }
+
+    private boolean isAllEnemiesDead(){
+        if (enemyNumber == 0) return true;
+        for(int a =0; a < enemyNumber; a++){
+            if(!enemy[a].isDead()) return false;
+        }
+        return true;
+    }
+
+    private void attackEnemy(int amount){
+        int index = 0; //may be out of bounds
+        for(int i=0; i < enemyNumber; i++){
+            index = i;
+            if(!enemy[i].isDead()) break;
+        }
+        System.out.println("enemy with index" + index +" got damage");
+        enemy[index].increaseOrDecreaseHealth(amount);
     }
 
     public void gift(){
-
+        System.out.println("Nothing happens here.");
     }
 }
